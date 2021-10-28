c56be76d446765b188e9843ce61da7eeeac8467e
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2021-10-27 21:29:48 -0300

    test: add demoblaze tests

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # cypress-mochawesome
+
+## Instalação do ambiente de desenvolvimento:
+
+1. Git Bash (Git for Windows - mais leve)
+https://gitforwindows.org/
+
+2. Nodejs (node):
+https://nodejs.org/en/
+
+3. IDE VSCode (code):
+https://code.visualstudio.com/
+OBS: Pode usar qualquer IDE. Eclipse, InteliJ...fica a seu critério.
+
+## Instalação - Cypress (Teste de UI):
+1. Faça a instalação do cypress via linha de comando. Abra o terminal e digite
+
+```
+npm install cypress
+```
+
+2. Caso não queira utilizar o cypress pelo NPM, pode fazer o download direto do site: https://www.cypress.io/
+
+Link para download direto: https://download.cypress.io/desktop
+
+Basta baixar, extrair, executar o Cypress.exe e apontar para o diretório do projeto desejado na interface do cypress.
+Utilize a IDE para editar o código.
---
+++ cypress/integration/tarefa_ui/demoblaze.spec.js
@@ -0,0 +1,105 @@
+/// <reference types="cypress"/>
+
+describe('Cenário de teste: Testar as funcionalidades do Youtube.', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.demoblaze.com/index.html');
+  })
+
+  it.skip('Caso de teste: Abrir diálogo de cadastro ao clicar no botão', () => {
+    cy.get('#signin2').click();
+    cy.get('#signInModal > .modal-dialog > .modal-content').should('be.visible');
+  })
+
+  it.skip('Caso de teste: Registrar um usuario com sucesso', () => {
+    criarNovoUsuario({testarFalha: false});
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('Sign up successful.');
+    });
+  })
+
+  it.skip('Caso de teste: Falha ao tentar registrar um usuario com dados invalidos', () => {
+    criarNovoUsuario({testarFalha: true});
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('Please fill out Username and Password.');
+    });
+  })
+
+  it('Caso de teste: Falha ao tentar registrar um usuario já cadastrado', () => {
+    criarUsuarioFixo('Sign up successful.');
+    criarUsuarioFixo('This user already exist.');
+  })
+
+  it.skip('Caso de teste: Login na plataforma com sucesso', () => {
+    var userId = loginDemoblaze();
+    cy.get('#nameofuser').should('have.text', `Welcome ${userId}`);
+  })
+
+  it.skip('Caso de teste: Sair da conta do usuário e verificar', () => {
+    var userId = loginDemoblaze();
+    cy.get('#nameofuser').should('have.text', `Welcome ${userId}`);
+    cy.get('#logout2').click();
+    cy.get('#nameofuser').should('not.be.visible');
+    cy.get('#signin2').should('be.visible');
+  })
+})
+
+function criarNovoUsuario({testarFalha}) {
+
+  let horas = new Date().getHours().toString();
+  let minutos = new Date().getMinutes().toString();
+  let seg = new Date().getSeconds().toString();
+  let userId = horas + minutos + seg + '_userId';
+  let userPass = horas + minutos + seg + '_userPass';
+  let userInfo = [userId, userPass]
+
+  cy.get('#signin2').click();
+  cy.get('#signInModal > .modal-dialog > .modal-content').should('be.visible').wait(500);
+  cy.get('#sign-username').type(userId);
+  cy.get('#sign-password').type(userPass);
+  if(testarFalha) cy.get('#sign-username').clear();
+  cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary')
+    .click().wait(500);
+  return userInfo;
+
+}
+
+function criarUsuarioFixo(checkText) {
+  var alertText = 'null';
+  cy.on('window:alert', (text) => {
+    alertText = text;
+  });
+
+  let horas = new Date().getHours().toString();
+  let minutos = new Date().getMinutes().toString();
+  let userId = horas + minutos + '_userId';
+  let userPass = horas + minutos + '_userPass';
+  let userInfo = [userId, userPass]
+
+  cy.get('#signin2').click();
+  cy.get('#signInModal > .modal-dialog > .modal-content').should('be.visible').wait(500);
+  cy.get('#sign-username').clear();
+  cy.get('#sign-password').clear();
+  cy.get('#sign-username').type(userId);
+  cy.get('#sign-password').type(userPass);
+  cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary')
+    .click().wait(2000).then(() => expect(alertText).to.equal(checkText));
+
+  return userInfo;
+
+}
+
+function loginDemoblaze(){
+  var userInfos = criarNovoUsuario({testarFalha: false});
+  cy.get('#login2').click();
+  cy.get('#logInModal > .modal-dialog > .modal-content')
+    .should('be.visible')
+    .wait(500);
+  cy.get('#loginusername').type(userInfos[0]);
+  cy.get('#loginpassword').type(userInfos[1]);
+  cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary')
+    .click()
+    .wait(500);
+  return userInfos[0];
+}
+
